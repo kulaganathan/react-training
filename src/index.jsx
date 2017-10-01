be85294e37ee0167e973175a08f2a825e2d52bcf
kalai <eie.kalai@gmail.com>
2017-10-01 15:43:01 -0400

    Added webpack-dev-server hot reload

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Wrapper extends React.component{
+export default class Wrapper extends React.Component{
   render() {
     return (<h1>Hello World!!!</h1>);
   }
